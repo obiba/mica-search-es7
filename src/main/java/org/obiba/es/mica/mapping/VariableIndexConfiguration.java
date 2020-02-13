09c6a729f99b6021b2cd2dfa815a81bb02e3f979
meek0 <cedricgfont@gmail.com>
2020-02-13 16:46:29 -0500

    sets must not be analyzed

--- src/main/java/org/obiba/es/mica/mapping/VariableIndexConfiguration.java
+++ src/main/java/org/obiba/es/mica/mapping/VariableIndexConfiguration.java
@@ -73,6 +73,7 @@ public class VariableIndexConfiguration extends AbstractIndexConfiguration {
     createMappingWithoutAnalyzer(mapping, "variableType");
     createMappingWithoutAnalyzer(mapping, "valueType");
     createMappingWithoutAnalyzer(mapping, "nature");
+    createMappingWithoutAnalyzer(mapping, "sets");
 
     // attributes from taxonomies
     try {
